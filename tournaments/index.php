ae2093d25f8649ba4f8a09311a449f111fe67b42
danielguerra2017 <34104809+danielguerra2017@users.noreply.github.com>
2018-02-26 21:06:31 -0500

    Add files via upload

---
+++ tournaments/index.php
@@ -0,0 +1,132 @@
+<!DOCTYPE HTML>
+
+<?php
+    include ('../header.php');
+?>
+
+    <div class="section section-header">
+        <div class="parallax filter">
+            <div class="image"
+                style="background-image: url('../assets/img/background/bf1Pic9.jpg')">
+            </div>
+            <div class="container">
+                <div class="content">
+                    <div class="title-area">
+                        <h1 class="title-modern"></h1>
+                        <h2>Tournaments</h2>
+                    </div>
+
+                </div>
+
+            </div>
+        </div>
+    </div>
+
+
+    <div class="section">
+        <div class="container">
+            <div class="row">
+                <div class="title-area">
+                    <h2>PS4 BF1 Tournaments</h2>
+                    <div class="separator separator-danger">♦</div>
+                        <p class="description">
+                        <table class="upcomingTournamentsTable">
+                            <tr>
+                                <th class="upcomingTournamentsTdTh">Tournament</th>
+                                <th class="upcomingTournamentsTdTh">Start Date</th>
+                            </tr>
+                            <tr>
+                                <td class="upcomingTournamentsTdTh">
+                                    <table>
+                                        <tr>
+                                            <td style="padding: 2px;">
+                                                <a href="http://www.eyesonobjective.com/tournaments/frontlines/wodT8FlS1.php" class="btn btn-danger btn-fill btn-sm">View</a>
+                                            </td>
+                                            <td style="padding: 2px;">
+                                                T8 FL S1
+                                            </td>
+                                        </tr>
+                                    </table>
+                                </td>
+                                <td class="upcomingTournamentsTdTh">Feb 9</td>
+                            </tr>
+                            <tr>
+                                <td class="upcomingTournamentsTdTh">
+                                    <table>
+                                        <tr>
+                                            <td style="padding: 2px;">
+                                                <a href="http://www.eyesonobjective.com/tournaments/domination/wodT5DomS2.php" class="btn btn-danger btn-fill btn-sm">View</a>
+                                            </td>
+                                            <td style="padding: 2px;">
+                                                T5 DOM S2: The Takeover
+                                            </td>
+                                        </tr>
+                                    </table>
+                                </td>
+                                <td class="upcomingTournamentsTdTh">Coming Soon</td>
+                            </tr>
+                        </table>
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="section section-our-team-freebie">
+        <div class="parallax filter filter-color-black">
+            <div class="image" style="background-image:url('../assets/img/background/bf1Pic10.jpg')">
+            </div>
+            <div class="container">
+                <div class="content">
+                    <div class="row">
+                        <div class="title-area">
+                            <h2>Player of the Week</h2>
+                            <div class="separator separator-danger">♦</div>
+                            <div id="mvpDiv">
+                                <p id="mvpBox"><span style="color: #DF0101; font-weight: bold;">Feb 23:</span> Player of the week is <span style="color: #DF0101; font-weight: bold;">Shepherd2420</span> for leading his team into a hard fought battle in the T8 FL Semi-Finals! 
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="section">
+        <div class="container">
+            <div class="row">
+                <div class="title-area">
+                    <h2>Tournament Archives</h2>
+                    <div class="separator separator-danger">♦</div>
+                        <p class="description">
+                        <table class="upcomingTournamentsTable">
+                            <tr>
+                                <th class="upcomingTournamentsTdTh">Tournament</th>
+                                <th class="upcomingTournamentsTdTh">Winner</th>
+                            </tr>
+                            <tr>
+                                <td class="upcomingTournamentsTdTh">
+                                    <table>
+                                        <tr>
+                                            <td style="padding: 2px;">
+                                                <a href="http://www.eyesonobjective.com/tournaments/domination/wodT5DomS1.php" class="btn btn-danger btn-fill btn-sm">View</a>
+                                            </td>
+                                            <td style="padding: 2px;">
+                                                T5 DOM S1: All Out War
+                                            </td>
+                                        </tr>
+                                    </table>
+                                </td>
+                                <td class="upcomingTournamentsTdTh">[dF] Defiance Gaming</td>
+                            </tr>
+                        </table>
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+<?php
+    include ('../footer.php');
+?>
