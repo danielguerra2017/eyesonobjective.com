329742c1bdeb665a3b1da5d62905733adba8c730
danielguerra2017 <34104809+danielguerra2017@users.noreply.github.com>
2018-02-26 20:45:48 -0500

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,88 @@
+<footer class="footer footer-big footer-color-black" data-color="black">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-2 col-sm-3">
+                    <div class="info">
+                        <h5 class="title"></h5>
+                        <nav>
+                            <ul>
+                            </ul>
+                        </nav>
+                    </div>
+                </div>
+                <div class="col-md-3 col-md-offset-1 col-sm-3">
+                    <div class="info">
+                        <h5 class="title">Help and Support</h5>
+                         <nav>
+                            <ul>
+                                <li>
+                                    <a href="mailto:eyesonobjective@gmail.com?Subject=WOD%20Question">Contact Us</a>
+                                </li>
+                                <li>
+                                    <a href="mailto:eyesonobjective@gmail.com?Subject=Sponsor%20a%20Tournament">Sponsor a Tournament</a>
+                                </li>
+                                <li>
+                                    <a href=""></a>
+                                </li>
+                            </ul>
+                        </nav>
+                    </div>
+                </div>
+                <div class="col-md-3 col-sm-3">
+                    <div class="info">
+                        <h5 class="title">Latest News</h5>
+                        <nav>
+                            <ul>
+                                <li>
+                                    <a href="https://www.facebook.com/groups/1816259062005637/">
+                                        <i class="fa fa-facebook-square"></i> <strong>Fight For The Cup!</strong> The T8 FL S1 tournament Championship match is set for this weekend! dF vs NAC will battle it out to see who is the true champ!
+                                        <hr class="hr-small">
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="https://www.facebook.com/groups/1816259062005637/">
+                                        <i class="fa fa-facebook-square"></i>We thank our community for all of their support!
+                                    </a>
+                                </li>
+                            </ul>
+                        </nav>
+                    </div>
+                </div>
+                <div class="col-md-2 col-md-offset-1 col-sm-3">
+                    <div class="info">
+                        <h5 class="title">Follow us on</h5>
+                        <nav>
+                            <ul>
+                                <li>
+                                    <a href="https://www.facebook.com/groups/1816259062005637/" class="btn btn-social btn-facebook btn-simple">
+                                        <i class="fa fa-facebook-square"></i> Facebook
+                                    </a>
+                                </li>
+                            </ul>
+                        </nav>
+                    </div>
+                </div>
+            </div>
+            <hr>
+            <div class="copyright">
+                 © <script> document.write(new Date().getFullYear()) </script> Win Or Die
+            </div>
+        </div>
+    </footer>
+
+</body>
+
+<!--   core js files    -->
+<script src="../../assets/js/jquery.min.js" type="text/javascript"></script>
+<script src="../../assets/js/bootstrap.js" type="text/javascript"></script>
+
+<!--  js library for devices recognition -->
+<script type="text/javascript" src="../../assets/js/modernizr.js"></script>
+
+<!--  script for google maps   -->
+<script type="text/javascript" src="https://maps.googleapis.com/maps/api/js"></script>
+
+<!--   file where we handle all the script from the Gaia - Bootstrap Template   -->
+<script type="text/javascript" src="../../assets/js/gaia.js"></script>
+
+</html>
