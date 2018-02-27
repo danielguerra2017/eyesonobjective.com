51d477467804685e0ec8494e03458592308ee72a
danielguerra2017 <34104809+danielguerra2017@users.noreply.github.com>
2018-02-26 21:03:55 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,148 @@
+<!DOCTYPE HTML>
+
+<?php
+    include ('header.php');
+?>
+
+    <div class="section section-header">
+        <div class="parallax filter">
+            <div class="image"
+                style="background-image: url('assets/img/background/bf1Pic4.jpg')">
+            </div>
+            <div class="container">
+                <div class="content">
+                    <div class="title-area">
+                        <h1 class="title-modern"></h1>
+                        <h2>Competitive Battlefield 1 Tournaments</h2>
+                    </div>
+
+                </div>
+
+            </div>
+        </div>
+    </div>
+
+
+    <div class="section">
+        <div class="container">
+            <div class="row">
+                <div class="title-area">
+                    <h2>News & Updates</h2>
+                    <div class="separator separator-danger">♦</div>
+                    <p id="updateBox"><span class="boldDate">Feb 24:</span> Stay up to date with all the action going on at Win Or Die! Join us on our Discord channel and join our rapidly growing community! Just <a href="https://discord.gg/3vwmf6U">click here</a> for the link!
+                    </p>
+                    <p id="updateBox"><span class="boldDate">Feb 23:</span> T8 FL S1 Finals are this Friday! Join us on the livestream and watch all the action as dF and NAC go head to head to each try to claim the crown as their own.
+                    </p>
+                    <p id="updateBox"><span class="boldDate">Feb 3:</span> Congratulations to [dF] Defiance Gaming for becoming the Champs of the T5 DOM S1 tournament and becoming the first ever Win Or Die Champs! Good job to both teams for a very hard fought battle.
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="section section-our-team-freebie">
+        <div class="parallax filter filter-color-black">
+            <div class="image" style="background-image:url('assets/img/background/bf1Pic1.jpg')">
+            </div>
+            <div class="container">
+                <div class="content">
+                    <div class="row">
+                        <div class="title-area">
+                            <h2>Upcoming Tournaments</h2>
+                            <div class="separator separator-danger">♦</div>
+                    <p class="description">
+                        <table class="upcomingTournamentsTable">
+                            <tr>
+                                <th class="upcomingTournamentsTdTh">Tournament</th>
+                                <th class="upcomingTournamentsTdTh">Start Date</th>
+                            </tr>
+                            <tr>
+                                <td class="upcomingTournamentsTdTh">
+                                    <table>
+                                        <tr>
+                                            <td style="padding: 2px;">
+                                                <a href="http://www.eyesonobjective.com/tournaments/frontlines/wodT8FlS1.php" class="btn btn-danger btn-fill btn-sm">View</a>
+                                            </td>
+                                            <td style="padding: 2px;">
+                                                T8 FL S1
+                                            </td>
+                                        </tr>
+                                    </table>
+                                </td>
+                                <td class="upcomingTournamentsTdTh">Feb 9</td>
+                            </tr>
+                            <tr>
+                                <td class="upcomingTournamentsTdTh">
+                                    <table>
+                                        <tr>
+                                            <td style="padding: 2px;">
+                                                <a href="http://www.eyesonobjective.com/tournaments/domination/wodT5DomS2.php" class="btn btn-danger btn-fill btn-sm">View</a>
+                                            </td>
+                                            <td style="padding: 2px;">
+                                                T5 DOM S2: The Takeover
+                                            </td>
+                                        </tr>
+                                    </table>
+                                </td>
+                                <td class="upcomingTournamentsTdTh">Coming Soon</td>
+                            </tr>
+                        </table>
+                    </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="section">
+        <div class="container">
+            <div class="row">
+                <div class="title-area">
+                    <h2>Our Mission</h2>
+                    <div class="separator separator-danger">♦</div>
+                        <p class="description">The competitive Battlefield scene is just around the corner. At WOD we intend to jumpstart the competitive scene by hosting open entry tournaments for those looking to elevate their game to the next level! Compete with a league that knows how a tournament should be run. Compete at <span style="color: #DF0101; font-weight: bold;">Win Or Die</span>.</p>
+                </div>
+            </div>
+                <div class="col-md-6">
+                    <div class="info-icon">
+                        <div class="icon text-danger">
+                        </div>
+                        <h3>Who</h3>
+                        <p class="description">Our staff is experienced and well seasonsed in competitive gameplay. We hope to combine our knowledge with the community's experiences inorder to create an arena the community wants to compete in.</p>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <div class="info-icon">
+                        <div class="icon text-danger">
+                        </div>
+                        <h3>Goal</h3>
+                        <p class="description">Our goal is to provide the best competitive gaming experience to the Battlefield community. In doing this, we will be looking to acquire sponsors as well as eventually hosting tournaments of all different game modes and sizes with prizes for the top teams.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="section section-small section-get-started">
+        <div class="parallax filter">
+            <div class="image"
+                style="background-image: url('assets/img/background/bf1Pic5.jpg')">
+            </div>
+            <div class="container">
+                <div class="title-area">
+                    <h2 class="text-white">Have Questions?</h2>
+                    <div class="separator line-separator">♦</div>
+                    <p class="description">If you have any questions or comments, please feel free to contact us. We will get back to you as soon as possible!</p>
+                </div>
+
+                <div class="button-get-started">
+                    <a href="mailto:eyesonobjective@gmail.com?Subject=WOD%20Question" class="btn btn-danger btn-fill btn-lg">Contact Us</a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+<?php
+    include ('footer.php');
+?>
